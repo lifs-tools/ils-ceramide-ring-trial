eb1411050c1bbde743d08d6bd2d9b92420a987c8
Bo Burla <bo.burla@gmail.com>
2024-08-10 15:14:27 +0800

    cleanup

--- evaluation.R
+++ evaluation.R
@@ -5,17 +5,20 @@ library(targets)
 library(gt)
 library(parallel)
 library(here)
+
+
+
 # use the following target to delete all intermediate results
 #tar_destroy(ask = FALSE)
 
 # use the following make target to allow browser() and other statements
 #tar_make(callr_function = NULL)
-
+# 
 # enable the following line to print details on warnings or on errors 
 #targets::tar_meta(fields = warnings, complete_only = TRUE)
 #targets::tar_meta(fields = error, complete_only = TRUE)
 tar_option_set(memory = "transient", garbage_collection = TRUE)
 # use the following command to run independent parts of the workflow in parallel
 tar_make_future(workers = min(4, parallel::detectCores() - 1), garbage_collection = TRUE)
-#tar_make_future(workers = 1)
+# 1#tar_make_future(workers = 1)
 #tar_glimpse()
